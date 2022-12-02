94a0193646f4ab586d87e32d9265eed0b4b8ad45
felix <fellix.la.roux/@gmail.com>
2022-12-02 22:27:40 +0400

    modified:   src/components/Vortex/Vortex.js

--- src/components/Vortex/Vortex.js
+++ src/components/Vortex/Vortex.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import UserContext from '../Hooks/UserContext';
 import useWindowSize from '../Hooks/useWindowSize';
 import useOperationSystem from '../Hooks/useOperationSystem';
